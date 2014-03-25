263ff08ac09453b78e881c23d3117a347aaafdbb
Giovanni Nunes <giovanni.nunes@gmail.com>
2014-03-25 09:35:32 -0300

    Update estante.php

--- rotinas/estante.php
+++ rotinas/estante.php
@@ -6,7 +6,7 @@
 
   Rotinas Específicas do Programa
   
-  Copyright (C) 2014  Giovanni Nunes <giovanni.nunes@gmail.com>
+  Copyright (C) 2005-2014  Giovanni Nunes <giovanni.nunes@gmail.com>
 
   This program is free software; you can redistribute it and/or modify it under
   the terms of the GNU General Public License as published by the the Free
